a5f683859184206136ff929a9ecd9cbd6ffbe009
atayev2012 <atayev2012@gmail.com>
2022-04-01 16:30:57 +0500

    Final Version of Homework 7 - Reading & writing files. Task 1 & 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def recipe_read(file_name:str) -> dict:
+def recipe_read_from_file(file_name:str) -> dict:
     cook_book = {}
     bool_switch = True
     file = open(file_name, 'rt', encoding='utf-8')
@@ -17,11 +17,23 @@ def recipe_read(file_name:str) -> dict:
             temp = line.strip().split(' | ')
             cook_book[temp_key] += [{'ingredient_name': temp[0], 'quantity': int(temp[1]), 'measure': temp[2]}]
             counter -= 1
-
     file.close()
     return cook_book
 
 
+def get_shop_list_by_dishes(dishes:list, person_count:int) -> dict:
+    cook_book = recipe_read_from_file('recipes.txt')
+    ingredients = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] in list(ingredients.keys()):
+                ingredients[ingredient['ingredient_name']]['quantity'] += person_count * ingredient['quantity']
+            else:
+                ingredients.setdefault(ingredient['ingredient_name'], {'measure': ingredient['measure'],'quantity':person_count * ingredient['quantity']})
+    return ingredients
 
 
-print(recipe_read('recipes.txt'))
+#Testing if the results
+print(get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
+print(get_shop_list_by_dishes(['Омлет'], 4))
+print(get_shop_list_by_dishes(['Фахитос'], 3))
